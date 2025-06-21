d2d040f40e1c94c763b8e836d8b9d2d644c0bce3
Alan <144842474+SeccreDev@users.noreply.github.com>
2025-06-20 22:22:56 -0400

    feat: add default system_prompt

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
     user_prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt))
 
     print(response.text)
     if verbose == "--verbose":
